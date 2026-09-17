3d56f8211dd077cdefe308efea0db48e016fbbdf
efremandrei <145491486+efremandrei@users.noreply.github.com>
2026-09-17 12:46:49 +0300

    fix: resolve live source build errors

--- app/src/main/java/com/efremushkin/magnetharbor/data/source/LiveTorrentSources.kt
+++ app/src/main/java/com/efremushkin/magnetharbor/data/source/LiveTorrentSources.kt
@@ -3,6 +3,7 @@ package com.efremushkin.magnetharbor.data.source
 import android.util.Xml
 import com.efremushkin.magnetharbor.data.model.TorrentCategory
 import com.efremushkin.magnetharbor.data.model.TorrentResult
+import com.efremushkin.magnetharbor.data.model.fromSourceValue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONArray
